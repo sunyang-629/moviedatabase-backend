849b61bcf4a676f683c0a53197fce1a6386d3943
sunyang9876 <sunyang9876@hotmail.com>
2020-09-16 14:28:15 +1000

    add utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // url
+  BASE_URL: 'https://api.themoviedb.org/3',
+  POSTER_URL: 'https://image.tmdb.org/t/p/w500',
+  BACKDROP_URL: 'https://image.tmdb.org/t/p/w1280',
+
+};
